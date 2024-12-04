a76d9e19674d40dca18138d1c9163e921759eead
joaolrech <158629556+joaolrech@users.noreply.github.com>
2024-12-04 22:11:50 +0000

    04/12/2024

--- backup.py
+++ backup.py
@@ -3,11 +3,11 @@ import os
 def inptintfix():
     while True:
         try:
-            inp = int(input())
-            if inp < 0 or inp > 2:
-                raise ValueError
-            return inp
-        except ValueError:
+            inpt = int(input())
+            if inpt < 0 or inpt > 2:
+                raise
+            return inpt
+        except:
             os.system("clear")
             print('Entrada inválida. Tente novamente.\n')
             printmenu()
@@ -15,11 +15,11 @@ def inptintfix():
 def inptintvar(quant, lista):
     while True:
         try:
-            inp = int(input())
-            if inp < 0 or inp > quant:
-                raise ValueError
-            return inp
-        except ValueError:
+            inpt = int(input())
+            if inpt < 0 or inpt > quant:
+                raise
+            return inpt
+        except:
             os.system("clear")
             print('Entrada inválida. Tente novamente.\n')
             listar(lista)
@@ -27,11 +27,11 @@ def inptintvar(quant, lista):
 def inptint(desc):
     while True:
         try:
-            inp = int(input(desc))
-            if inp < 1:
-                raise ValueError
-            return inp
-        except ValueError:
+            inpt = int(input(desc))
+            if inpt < 1:
+                raise
+            return inpt
+        except:
             print('Entrada inválida. Tente novamente.')
 
 def printmenu():
@@ -43,32 +43,28 @@ def printmenu():
 def listar(lista):
     if lista == disponiveis:
         print('Veículos disponíveis:')
-        for i, (veiculo, preco) in enumerate(disponiveis.items(), start = 1):
+        for i, (veiculo, preco) in enumerate(disponiveis, start = 1):
             print(f'{i} - {veiculo} - R${preco:.2f}/dia')
     if lista == locados:
         if locados:
             print('Veículos locados:')
-            for i, veiculo in enumerate(locados, start = 1):
+            for i, (veiculo, preco) in enumerate(locados, start = 1):
                 print(f'{i} - {veiculo}')
         else:
             print('Nenhum veículo está locado.')
     print('\n0 - VOLTAR \n')
 
-disponiveis = {
-    'Dolphin Mini' : 87.06,
-    'Dolphin' : 116.72,
-    'Dolphin Plus' : 124.54,
-    'Han' : 405.83,
-    'King' : 124.05,
-    'Seal' : 197.92,
-    'Shark' : 306.42,
-    'Song Plus' : 169.75,
-    'Song Pro' : 133.06,
-    'Tan' : 374.14,
-    'Yuan Plus' : 155.51,
-    'Yuan Pro' : 132.2,
-    }
-locados = {}
+disponiveis = [
+    ('Dolphin', 87.06),
+    ('Han', 405.83),
+    ('King', 124.05),
+    ('Seal', 197.92),
+    ('Shark', 306.42),
+    ('Song', 133.06),
+    ('Tan', 374.14),
+    ('Yuan', 132.2),
+]
+locados = []
 
 os.system("clear")
 print('Bem vindo à locadora.py! \n')
@@ -85,21 +81,21 @@ while True:
             os.system("clear")
             if escolha == 0:
                 continue
-            veiculolocado = list(disponiveis.keys())[escolha - 1]
+            veiculolocado = disponiveis[escolha - 1][0]
             print(f'{veiculolocado} locado. \n')
-            locados[veiculolocado] = disponiveis.pop(veiculolocado)
+            locados.append(disponiveis.pop(escolha - 1))
         case 2:
             listar(locados)
             escolha = inptintvar(len(locados), locados)
             os.system("clear")
             if escolha == 0:
                 continue
-            veiculodevolvido = list(locados.keys())[escolha - 1]
+            veiculodevolvido = locados[escolha - 1][0]
             dias = inptint(f'Digite quantos dias você ficou com o {veiculodevolvido}: ')
             os.system("clear")
             print(f'{veiculodevolvido} devolvido.')
-            print(f'Valor em haver: R${locados[veiculodevolvido] * dias:.2f} \n')
-            disponiveis[veiculodevolvido] = locados.pop(veiculodevolvido)
+            print(f'Valor em haver: R${locados[escolha - 1][1] * dias:.2f} \n')
+            disponiveis.append(locados.pop(escolha - 1))
         case 0:
             break
 
