d54341a13794ee10db0957fd86ba62ed8a812a9a
joaolrech <158629556+joaolrech@users.noreply.github.com>
2024-12-05 22:28:39 +0000

    05/12/2024

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,41 +1,57 @@
 import os
 import random
 
-def inptjogada(desc):
+def inptcaracter(desc):
     while True:
         inpt = input(desc).upper()
         if inpt not in ['X', 'O']:
             os.system("clear")
             print('Entrada inválida. Tente novamente\n')
         else:
+            os.system('clear')
             return inpt
-    os.system('clear')
-    if inpt == 'O':
-        pc = 'X'
-    else:
-        inpt = 'O'
 
 def printjogo(s):
     print(f'{s[0]} {s[1]} {s[2]}')
     print(f'{s[3]} {s[4]} {s[5]}')
     print(f'{s[6]} {s[7]} {s[8]}')
 
-def printopcoes():
-    print('Selecione a posição em que deseja jogar.\n')
-    print('0 - FINALIZAR')
-
-
+def inptjogada():
+    while True:
+        try:
+            print('Selecione a posição em que deseja jogar.\n')
+            printjogo(situacao)
+            print('\n0 - FINALIZAR\n')
+            inpt = int(input())
+            if inpt < 0 or inpt > 10:
+                raise
+            return inpt
+        except:
+            os.system("clear")
+            print('Entrada inválida. Tente novamente.\n')
 
 situacao = [i + 1 for i in range(9)]
 
 os.system('clear')
 print('Bem vindo ao jogo da velha! \n')
-us = inptjogada('Deseja ser X ou O?\n\n')
+us = inptcaracter('Deseja ser X ou O?\n\n')
+if us == 'O':
+    pc = 'X'
+else:
+    pc = 'O'
+
+inptjogada()
+
+os.system('clear')
+
 
 
 
 
 while True:
+    print(us)
+    print(pc)
+    print(situacao)
     printjogo(situacao)
     break
 
